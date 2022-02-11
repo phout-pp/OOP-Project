089e951a08a48be5f6faac91bd86721a54a6728b
phout-pp <phout001@gmail.com>
2022-02-11 17:44:02 +0700

    Student fix1

--- allPackage/Students.java
+++ allPackage/Students.java
@@ -1,9 +1,4 @@
 package allPackage;
 public class Students extends Select {
     protected int Year;
-
-    public Students(int year){
-        super(4, "Laos");
-        this.Year = year; 
-    }
 }
